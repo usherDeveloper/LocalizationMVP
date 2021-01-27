f0366abb7b1ba36cfde41cf082b9744aed1d50b6
Sachindra Pandey <sachinpandey44@gmail.com>
2021-01-27 16:38:01 +0800

    Initial Commit

---
+++ LocalizationMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizationMVP
+//
+//  Created by TechM on 27/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
